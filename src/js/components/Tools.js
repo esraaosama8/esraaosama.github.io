c95465343eeb778e1fda61bb68cb73bd9ed69b09
Esraa Osama <34555796+esraaosama8@users.noreply.github.com>
2020-05-28 12:24:09 +0200

    Added tools

--- src/js/components/Tools.js
+++ src/js/components/Tools.js
@@ -5,29 +5,42 @@ class Tools extends React.Component {
             <table className="table table-bordered table-striped">
 
                 <tbody>
+                 <tr>
+            <td>Adobe XD</td>
+            </tr>
+                <tr>
+        <td>Axure</td>
+        </tr>
+                <tr>
+        <td>Balsamiq</td>
+        </tr>
+                 <tr>
+                    <td>Bootstrap 4</td>
+                </tr>
+                <tr>
+                    <td>CSS3</td>
+                </tr>
                 <tr>
                     <td>HTML5</td>
                 </tr>
-                <tr>
-                    <td>CSS</td>
-                </tr>
-                <tr>
-                    <td>Bootstrap</td>
-                </tr>
-            <tr>
-            <td>Adobe XD</td>
-            </tr>
-        <tr>
-        <td>Axure</td>
-        </tr>
-        <tr>
-        <td>Balsamiq</td>
-        </tr>
-        <tr>
+                  <tr>
         <td>InVision</td>
         </tr>
+                         <tr>
+        <td>Reactjs</td>
+        </tr>
+                         <tr>
+        <td>React Native</td>
+        </tr>
+                         <tr>
+        <td>Responsive Web Design</td>
+        </tr>
+  
         <tr>
         <td>User Interface Prototyping</td>
+        </tr>
+                         <tr>
+        <td>Wireframes</td>
         </tr>
 
                 </tbody>
