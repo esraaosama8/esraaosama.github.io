0afcd76699e6ed261ecc58a891a272825ac7380f
Esraa Osama <34555796+esraaosama8@users.noreply.github.com>
2020-08-14 14:28:59 +0200

    Update Bio.js

--- src/js/components/Bio.js
+++ src/js/components/Bio.js
@@ -3,10 +3,11 @@ class Bio extends React.Component {
         return (<div className="card mt-3">
             <div className="card-body">
                 <p className="card-text">
-I'm a fresh graduate from Cairo University Faculty of Computer Science and Artificial Intelligence. Looking for internships or full-time jobs in Web Development or UI/UX Design to work on what I've learnt so far through the courses I've been taking online. I dedicate my time and become committed to any tasks given to me.
-Also hopefully I would be able to learn new technologies and features if needed for any job. 
 
 
+I'm a fresh graduate from Cairo University Faculty of Computer Science and AI. I'm looking for internships or part-time jobs in different fields if applicable.
+Want to work on what I've learned so far through the courses I've been taking. Hopefully, I would be able to learn new technologies and features if needed for any job. 
+I dedicate my time and commit to any tasks given to me.
 
                 </p>
             </div>
